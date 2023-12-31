1ac5c82ab0c6d5cffe0ac0b63d18b91994a071ff
Noman <noman@noman.land>
2023-12-31 04:50:35 -0500

    Keep score

--- main.lua
+++ main.lua
@@ -2,16 +2,17 @@ function _init()
 	frame = 0
 end
 
-function _update60()
-	frame += 0.5
+function _update()
+	frame += 1
 	reload:check()
-	complete:check(sil_guy)
+	complete:check(sil_guy, dance_guy)
 	sil_guy:update()
 	dance_guy:update()
 end
 
 function _draw()
 	background:draw()
+	score:draw()
 	sil_guy:draw()
 	ground:draw()
 	dance_guy:draw()
--- text.lua
+++ text.lua
@@ -3,7 +3,7 @@ try_again = {
     return pressed_any "❎"
   end,
   draw = function()
-    print("❎ try again", 4, 119, 12)
+    print("❎ try again", 2, 121, 12)
   end
 }
 
@@ -16,23 +16,29 @@ reload = {
 complete = {
   level_won = false,
   is_over = false,
-  check = function(self, sil_guy)
+  check = function(self, sil_guy, dance_guy)
     local correctly_pressed = pressed_only(sil_guy.winning_move)
     local moment_of_overlap = flr(sil_guy.pos.y) == dance_guy.pos.y
     if correctly_pressed and moment_of_overlap then
       -- flash screen
       cls(14)
       self.level_won = true
+      score.score += 1
     end
     self.is_over = sil_guy.pos.y > dance_guy.pos.y
   end,
   draw = function(self)
     if self.level_won then
       print('great job!', 20, 40, 14)
-      print('score: 1', 20, 47, 14)
     elseif self.is_over then
       print('bad job :(', 70, 40, 8)
-      print('score: 0', 70, 47, 8)
     end
+  end
+}
+
+score = {
+  score = 0,
+  draw = function(self)
+    print("score: " .. self.score, 2, 2)
   end
 }
